3820a4b5fffe913d9585b0c4da00a0f033f60bc5
JAYANI. S <jayanisubramaniam@gmail.com>
2024-09-10 20:46:49 +0530

    Add files via upload

---
+++ Music Player/src/music.js
@@ -0,0 +1,22 @@
+let allMusic = [
+    {
+        name: "Run - Red(Taylor's Version)",
+        artist: "Taylor Swift & Ed Shreeran",
+        img: "run",
+        src: "run"
+    },
+
+    {
+        name: "Perfect - Divide",
+        artist: "Ed Shreeran",
+        img: "perfect",
+        src: "perfect"
+    },
+
+    {
+        name: "Heat Waves - Dreamland",
+        artist: "Glass Animals",
+        img: "heatwaves",
+        src: "heatwaves"
+    }
+]
